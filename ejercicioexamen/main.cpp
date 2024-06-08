04273dea9e0bd2cdcdd13b831b1773c08789c767
JOSE69999 <166670202+JOSE69999@users.noreply.github.com>
2024-06-08 10:09:50 -0600

    Add files via upload

---
+++ ejercicioexamen/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i;
+    int numero, suma = 0;
+    for (i = 0; i < 4; i++) {
+    }
+    cout << "Ingrese un numero: " << endl;
+    cin >> numero;
+    suma = suma + numero;
+    cout << "Total: " << suma;
+    return 0;
+}
